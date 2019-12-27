38b313124982324c1415f441248a97058653d2a7
Gabriel Soares <gabrielsxp@hotmail.com>
2019-12-27 18:52:26 -0300

    Pool of Fighters Created

--- src/Controller/RNGController.js
+++ src/Controller/RNGController.js
@@ -1,5 +1,8 @@
 const Srand = require('jsrand');
+const Summon = require('../Controller/UserController');
 const Fighter = require('../Model/Fighter');
+const fs = require('fs');
+const path = require('path');
 
 const LOWER_BRONZE = 0;
 const UPPER_BRONZE = 19;
@@ -8,25 +11,127 @@ const UPPER_SILVER = 93;
 const LOWER_GOLD = 94;
 const UPPER_GOLD = 99;
 
-function isBetween(n, lower, upper){
+/**
+ * Função responsável por verificar se um determinado número recebido como
+ * parâmetro está entre dois números definidos
+ * 
+ * @param {Number} n 
+ * @param {Number} lower 
+ * @param {Number} upper 
+ */
+function isBetween(n, lower, upper) {
     return (n >= lower && n <= upper);
 }
 
+/**
+ * Função responsável por construir uma Promise para o uso de async/await
+ * junto à função fs.readFile()
+ * 
+ * @param {String} path 
+ * @param {String} encoding 
+ */
+async function readFile(path, encoding) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(path, encoding, (err, data) => {
+            if (err) {
+                reject(err);
+            }
+            resolve(data);
+        });
+    });
+}
+
+/**
+ * Função responsável por construir uma Promise para o uso de async/await
+ * junto à função fs.writeFile()
+ * 
+ * @param {String} path 
+ * @param {String} content 
+ */
+async function writeFile(path, content){
+    return new Promise((resolve, reject) => {
+        fs.writeFile(path, content, (err) => {
+            if(err){
+                reject(err);
+            }
+            resolve({writed: true});
+        })
+    })
+}
+
+/**
+ * Retorna um lutador aleatório de um determinado tipo
+ * Obs: Inclui todos os lutadores existentes
+ * 
+ * @param {String} type 
+ */
+const randomFighter = async (type) => {
+    const validTypes = ['Gold', 'Bronze', 'Silver'];
+    const isValidType = validTypes.includes(type);
+    if (!isValidType) {
+        throw new Error('Invalid Fighter Type');
+    }
+
+    const fighterType = type.toLowerCase();
+    const dataPath = path.join(__dirname, '..', 'data', `${fighterType}.txt`);
+    const data = await readFile(dataPath, 'utf8');
+    if (!data) {
+        throw new Error('Inexistent Data');
+    }
+    let lines = data.split('\n');
+    const fighterId = lines[Srand.randInt(0, lines.length)];
+    const fighter = await Fighter.findById(fighterId);
+
+    return fighter;
+}
+
+/**
+ * Retorna um lutador baseado no intervalo de números, definindo a probabilidade
+ * fixa para cada tipo existente
+ */
+const typedFighter = async () => {
+    let n = Srand.randInt(0, 100);
+    let fighter = null;
+
+    if (isBetween(n, LOWER_BRONZE, UPPER_BRONZE)) {
+        fighter = await randomFighter('Bronze');
+    }
+    if (isBetween(n, LOWER_SILVER, UPPER_SILVER)) {
+        fighter = await randomFighter('Silver');
+    }
+    if (isBetween(n, LOWER_GOLD, UPPER_GOLD)) {
+        fighter = await randomFighter('Gold');
+    }
+
+    return fighter;
+}
+
+/**
+ * Define uma pool de lutadores, de modo que seja possível limitar quais lutadores
+ * serão extraídos a partir do algorítmo de números aleatórios
+ * 
+ * @param {String} username 
+ * @param {String} poolName 
+ * @param {Array} fighters
+ */
+const definePool = (username, poolName, fighters) => {
+    const poolPath = path.join(__dirname, "..", "users", `${username}`, `${poolName}.txt`);
+    console.log(poolPath);
+    
+}
+
 module.exports = {
-    async single(req, res){
-        let n = Srand.randInt(0, 100);
-        console.log(n);
-        if(isBetween(n, LOWER_BRONZE, UPPER_BRONZE)){
-            await Fighter.find({rarity: 'Bronze'}, (err, fighters) => {
-                if(err){
-                    return res.status(400).send({error: 'Unable to retrieve the fighters array'});
-                }
-                const size = fighters.length;
-                console.log(fighters);
-                console.log(size);
-                return res.status(200).send({fighter: fighters[Srand.randInt(0, size)]});
-            })
+    typedFighter,
+    async single(req, res) {
+        let fighter = await typedFighter();
+        return res.status(200).send({ fighter });
+    },
+    async multi(req, res) {
+        let fighters = [];
+        for (let i = 0; i < 10; i++) {
+            let fighter = await typedFighter(req, res);
+            fighters = fighters.concat(fighter);
         }
-        return res.status(200).send({fighter: null});
+        return res.status(200).send({ fighters });
     }
 }
