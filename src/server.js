e4d3b241a4a20eb1d1bf6098bdff11cd9dbbedcf
Gabriel Soares <gabrielsxp@hotmail.com>
2020-01-14 15:47:03 -0300

    Change Dot Env

--- src/server.js
+++ src/server.js
@@ -1,8 +1,5 @@
 const app = require('./app');
 const dotenv = require('dotenv').config();
-
-const port = process.env.SERVER_PORT || 3000;
-
 app.listen(port, () => {
     console.log('Server is running');
 })
